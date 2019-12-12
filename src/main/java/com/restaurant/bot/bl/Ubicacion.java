f7a3106b0144d9d851e599dc7ee9dfd236a52fc0
rami554 <menendez.ticona.ramiro@gmail.com>
2019-12-12 17:41:37 -0400

    Pruebas Unitarias de la Funcion Ubicacion

--- src/main/java/com/restaurant/bot/bl/Ubicacion.java
+++ src/main/java/com/restaurant/bot/bl/Ubicacion.java
@@ -2,21 +2,22 @@ package com.restaurant.bot.bl;
 //funcion principal para calcular distancia entre puntos
 
 public class Ubicacion {
-   /* public static double distance(){
+
+    public static double distance(double latp, double lonp, double lon, double latg){
         final int R = 6371; //radio total de la tierra;
 
         Double latDistance = Math.toRadians(latp - latg);
-        Double lonDistance = Math.toRadians(lonp - long);
+        Double lonDistance = Math.toRadians(lonp - lon);
         Double a = Math.sin(latDistance/2)*Math.sin(latDistance/2)
                 +Math.cos(Math.toRadians(latg))*Math.cos(Math.toRadians(latp))*Math.sin(lonDistance/2)*Math.sin(lonDistance/2);
         Double c= 2*Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
 
         double distance = R*c*1000; //Conversion a metros
 
-        double height = elg-elp;
+        //double height = elg-elp;
 
-        distance=Math.pow(distance, 2) + Math.pow(height, 2);
+        distance=Math.pow(distance, 2); //+ Math.pow(height, 2);
 
         return Math.sqrt(distance);
-    }*/
+    }
 }
---
+++ src/test/java/com/restaurant/bot/UbicacionTest.java
@@ -0,0 +1,26 @@
+package com.restaurant.bot;
+
+import com.restaurant.bot.bl.Ubicacion;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UbicacionTest {
+
+//    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void distancia(){
+        final Double expected = 6778557.561598529;
+
+        final Double actual = Ubicacion.distance(-16.52326,-68.11182, -16.47311, -68.15140);
+        System.out.println(actual);
+        Assert.assertEquals(actual, expected);
+    }
+}
